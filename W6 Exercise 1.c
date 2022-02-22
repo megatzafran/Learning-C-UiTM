9188e0f277c9d3d1ca32a116319e4de8f823049d
megatzafran <2021854946@student.uitm.edu.my>
2022-02-22 09:59:36 +0800

    lets goo go go go og

--- W6 Exercise 1.c
+++ W6 Exercise 1.c
@@ -2,34 +2,35 @@
 
 int main()
 {
-    int waterUsage;
+    float waterUsage;
     float waterBill;
     char userType;
 
     printf("User Type:\n");
-    printf("[A] Domestic Usage\n[B] Bulk meter");
-    printf("Insert your user type and water unit usage amount: ");
-    scanf("%c %f", &userType, &waterUsage);
+    printf("[A] Domestic Usage\n[B] Bulk meter\n\n");
+    printf("Insert your user type: ");
+    scanf(" %c", &userType);
+    printf("\nInsert your water unit usage amount: ");
+    scanf("%f", &waterUsage);
 
     if (userType == 'A')
     {
-        if (waterUsage < 100)
+        if (waterUsage <= 100)
         {
-            float rateUnit = 0.1;
-            waterBill = waterUsage * rateUnit;
-            printf("Your water bill is = %f", waterBill);
+            waterBill = waterUsage * 0.1;
+            printf("Your water bill is = %.2f", waterBill);
         }
         else if (waterUsage > 100 && waterUsage < 201)
         {
             float rateUnit = 0.5;
             waterBill = (waterUsage - 100) * 0.05 + (100 * 0.1);
-            printf("Your water bill is = %f", waterBill);
+            printf("Your water bill is = %.2f", waterBill);
         }
         else if (waterUsage >= 201)
         {
             float rateUnit = 0.5;
             waterBill = (waterUsage - 200 - 100) * 0.03 + (100 * 0.1) + (200 * 0.05);
-            printf("Your water bill is = %f", waterBill);
+            printf("Your water bill is = %.2f", waterBill);
         }
         else
         {
@@ -41,18 +42,18 @@ int main()
         if (waterUsage < 100)
         {
             waterBill = waterUsage * 0.4;
-            printf("Your water bill is = %f", waterBill);
+            printf("Your water bill is = %.2f", waterBill);
         }
         else if (waterUsage > 100 && waterUsage < 201)
         {
             waterBill = (waterUsage - 100) * 0.03 + (100 * 0.4);
-            printf("Your water bill is = %f", waterBill);
+            printf("Your water bill is = %.2f", waterBill);
         }
         else if (waterUsage >= 201)
         {
             float rateUnit = 0.5;
             waterBill = (waterUsage - 200 - 100) * 0.1 + (100 * 0.4) + (200 * 0.3);
-            printf("Your water bill is = %f", waterBill);
+            printf("Your water bill is = %.2f", waterBill);
         }
         else
         {
