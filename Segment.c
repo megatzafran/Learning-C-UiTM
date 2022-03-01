fde04975838e5e8cd95f85ddfd2561ce34d25fd0
megatzafran <2021854946@student.uitm.edu.my>
2022-03-01 09:32:00 +0800

    Last

--- Segment.c
+++ Segment.c
@@ -11,34 +11,35 @@ int main()
     printf("[1] Industrial\n");
 
     printf("Insert your type:");
-    scanf("%d", type);
-    if (type == 1)
+    scanf("%d", &type);
+    if (type == 0)
     {
-        printf("Insert electricity usage");
-        scanf("%d", electricityUsage);
+        printf("\nInsert electricity usage: ");
+        scanf("%d", &electricityUsage);
         if (electricityUsage > 0 && electricityUsage <= 200)
         {
             totalPayment = electricityUsage * 0.2;
-            printf("%f", totalPayment);
+            printf("%.2f", totalPayment);
         }
         else if (electricityUsage > 200 && electricityUsage < 300)
         {
             totalPayment = 200 * 0.2 + (electricityUsage - 200) * 0.3;
-            printf("%f", totalPayment);
+            printf("%.2f", totalPayment);
         }
     }
-    else if (type == 2)
+    else if (type == 1)
     {
-        printf("Insert electricity usage");
-        if (electricityUsage <= 200 && electricityUsage > 0)
+        printf("Insert electricity usage: ");
+        scanf("%d", &electricityUsage);
+        if (electricityUsage > 0 && electricityUsage <= 200)
         {
             totalPayment = electricityUsage * 0.5;
-            printf("%f", totalPayment);
+            printf("%.2f", totalPayment);
         }
         else if (electricityUsage > 200 && electricityUsage < 300)
         {
             totalPayment = 200 * 0.5 + (electricityUsage - 200) * 0.65;
-            printf("%f", totalPayment);
+            printf("%.2f", totalPayment);
         }
     }
     else
---
+++ Segment4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int condition;
+
+    if (condition)
+    {
+
+        if (condition)
+        {
+        }
+        else if (condition)
+        {
+        }
+    }
+    else if (condition)
+    {
+    }
+    else if (condition)
+    {
+    }
+}
