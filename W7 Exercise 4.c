151a95333db6fc8ba12aa3673669c71aafecf0f3
megatzafran <2021854946@student.uitm.edu.my>
2022-03-23 21:16:45 +0800

    hardest by far

---
+++ W7 Exercise 4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int cycle = 1;
+    float product;
+    float priceS, priceC, priceM;
+    float discountPriceS, discountPriceC, discountPriceM;
+
+    printf("Product Type\tDiscount Rate\n");
+    printf("[S]\t\t0.3\n");
+    printf("[C]\t\t0.1\n");
+    printf("[M]\t\t0.4\n");
+
+    while (cycle < 4)
+    {
+        if (cycle == 1)
+        {
+            printf("\nWhat is the price of product S: ", product);
+            scanf("%f", &priceS);
+
+            discountPriceS = priceS - (priceS * 0.3);
+        }
+        else if (cycle == 2)
+        {
+            printf("\nWhat is the price of product C: ", product);
+            scanf("%f", &priceC);
+
+            discountPriceC = priceC - (priceC * 0.1);
+        }
+        else
+        {
+            printf("\nWhat is the price of product M: ", product);
+            scanf("%f", &priceM);
+
+            discountPriceM = priceM - (priceM * 0.4);
+        }
+
+        cycle++;
+    }
+
+    printf("\fPrice after discount is:\n");
+    printf("S : %.2f\n", discountPriceS);
+    printf("C : %.2f\n", discountPriceC);
+    printf("M : %.2f\n", discountPriceM);
+}
