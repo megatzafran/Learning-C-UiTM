daf66a62f2b7a1a9a6708a3eb750411fdaea0772
megatzafran <2021854946@student.uitm.edu.my>
2022-03-24 13:14:18 +0800

    Lab Exercise 3 done

--- W7 Exercise 5.c
+++ W7 Exercise 5.c
@@ -1,13 +1,54 @@
 #include <stdio.h>
+#define YES 'y'
 
 int main()
 {
-    char i;
-    for (i = 'a'; i <= 'z'; i++)
+
+    char keepContinue = 'y', cycle = 1;
+    float product;
+    float priceS, priceC, priceM;
+    float discountPriceS, discountPriceC, discountPriceM;
+
+    while (keepContinue == 'y')
     {
-        if (i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u')
+        printf("\nProduct Type\tDiscount Rate\n");
+        printf("[S]\t\t0.3\n");
+        printf("[C]\t\t0.1\n");
+        printf("[M]\t\t0.4\n");
+
+        while (cycle < 4)
         {
-            printf("%c", i);
+            if (cycle == 1)
+            {
+                printf("\nWhat is the price of product S: ", product);
+                scanf("%f", &priceS);
+
+                discountPriceS = priceS - (priceS * 0.3);
+            }
+            else if (cycle == 2)
+            {
+                printf("\nWhat is the price of product C: ", product);
+                scanf("%f", &priceC);
+
+                discountPriceC = priceC - (priceC * 0.1);
+            }
+            else
+            {
+                printf("\nWhat is the price of product M: ", product);
+                scanf("%f", &priceM);
+
+                discountPriceM = priceM - (priceM * 0.4);
+            }
+
+            cycle++;
         }
+        printf("\fPrice after discount is:\n");
+        printf("S : %.2f\n", discountPriceS);
+        printf("C : %.2f\n", discountPriceC);
+        printf("M : %.2f\n", discountPriceM);
+
+        cycle = 1;
+        printf("\nDo you wish to proceed to another calculation? (y/n): ");
+        scanf(" %c", &keepContinue);
     }
 }
--- W7 Exercise 7.c
+++ W7 Exercise 7.c
@@ -3,13 +3,49 @@
 int main()
 {
     int i, j;
-    for (i = 4; i < 13; i += 4)
+    float number1, number2, number3, number4, number5;
+
+    for (i = 1; i < 6; i++)
     {
-        printf("x%d: ", i);
-        for (j = 1; j < 6; j++)
+        printf("\nInsert your number [%d]: ", i);
+        switch (i)
         {
-            printf("%d ", i * j);
+        case 1:
+            scanf("%f", &number1);
+            break;
+        case 2:
+            scanf("%f", &number2);
+            break;
+        case 3:
+            scanf("%f", &number3);
+            break;
+        case 4:
+            scanf("%f", &number4);
+            break;
+        case 5:
+            scanf("%f", &number5);
+            break;
         }
-        printf("\n");
+    }
+
+    if (number1 > number2 && number1 > number3 && number1 > number4 && number1 > number5)
+    {
+        printf("Number 1 is the maximum number with the value of %.2f", number1);
+    }
+    else if (number2 > number1 && number2 > number3 && number2 > number4 && number2 > number5)
+    {
+        printf("Number 2 is the maximum number with the value of %.2f", number2);
+    }
+    else if (number3 > number1 && number3 > number2 && number3 > number4 && number3 > number5)
+    {
+        printf("Number 3 is the maximum number with the value of %.2f", number3);
+    }
+    else if (number4 > number1 && number4 > number2 && number4 > number3 && number4 > number5)
+    {
+        printf("Number 4 is the maximum number with the value of %.2f", number4);
+    }
+    else
+    {
+        printf("Number 5 is the maximum number with the value of %.2f", number5);
     }
 }
