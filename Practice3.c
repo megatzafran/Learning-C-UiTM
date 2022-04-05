57a15c5cc2187bd6a3df1259c503b2305adbcaff
megatzafran <2021854946@student.uitm.edu.my>
2022-04-05 22:17:01 +0800

    done

--- Practice3.c
+++ Practice3.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    float price[3];
+    float price[3], totalPrice;
     int i;
 
     for (i = 0; i < 3; i++)
@@ -10,10 +10,14 @@ int main()
         printf("Enter a value: ");
         scanf("%f", &price[i]);
     }
+
     printf("\nList of price");
 
     for (i = 0; i < 3; i++)
     {
         printf("\n%.2f", price[i]);
-    }
+        totalPrice += price[i];
+        }
+
+    printf("\n\nTotal price : %.2f", totalPrice);
 }
--- W8 Exercise 2.c
+++ W8 Exercise 2.c
@@ -2,4 +2,21 @@
 
 int main()
 {
+    int i, numbers[5], temp;
+    for (i = 0; i < 5; i++)
+    {
+        printf("Insert number %d: ", i + 1);
+        scanf("%d", &numbers[i]);
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        temp = -9999;
+        if (temp < numbers[i])
+        {
+            temp = numbers[i];
+        }
+    }
+
+    printf("\nThe highest number is %d", temp);
 }
